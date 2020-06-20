64f48655123756a1e786af5e7506e132b8a7ce35
Chise1 <chise123@live.com>
2020-06-20 23:33:35 +0800

    v0.6

--- Service/__init__.py
+++ Service/__init__.py
@@ -9,5 +9,6 @@
 """
 import os
 BASE_URL="http://127.0.0.1:8000/Slaver/"
+# BASE_URL="http://39.98.132.68:8000/Slaver/"
 server_id= os.getenv('server_id') or  "0612c18a-b321-42cf-a2d1-09d1d4e1be1a"
 APPSERECT="560c52ccd288fed045859ed18bffd973"
